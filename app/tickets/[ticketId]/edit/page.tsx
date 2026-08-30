4e8276308c9ea9c44b869fc730e245d406809078
nemanja.mandic <nemanja.mandic@allwyn-lotterysolutions.com>
2026-08-30 18:24:09 +0200

    added breadcrumbs

--- app/tickets/[ticketId]/edit/page.tsx
+++ app/tickets/[ticketId]/edit/page.tsx
@@ -4,6 +4,9 @@ import { NotFound } from "@/features/components/NotFound/NotFound";
 import { TicketForm } from "@/features/ticket/components/TicketForm";
 import { getAuth } from "@/features/auth/queries/getAuth";
 import { isOwner } from "@/features/auth/utils/isOwner";
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import { homePath, ticketPath } from "@/app/utils/paths";
+import { Separator } from "@/components/ui/separator";
 
 type EditTicketPageProps = {
   params: Promise<{
@@ -23,13 +26,23 @@ export default async function EditTicketPage({ params }: EditTicketPageProps) {
   }
 
   return (
-    <div className="flex-1 flex flex-col justify-center items-center">
-      <CardCompact
-        className="w-full max-w-[420px] animate-fade-from-top"
-        title="Edit Ticket"
-        description="Edit an existing ticket"
-        content={<TicketForm ticket={ticketToEdit} />}
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Breadcrumbs
+        breadcrumbs={[
+          { title: "Tickets", href: homePath },
+          { title: ticketToEdit.title, href: ticketPath(ticketToEdit.id) },
+          { title: "Edit" },
+        ]}
       />
+      <Separator />
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <CardCompact
+          className="w-full max-w-[420px] animate-fade-from-top"
+          title="Edit Ticket"
+          description="Edit an existing ticket"
+          content={<TicketForm ticket={ticketToEdit} />}
+        />
+      </div>
     </div>
   );
 }
--- app/tickets/[ticketId]/page.tsx
+++ app/tickets/[ticketId]/page.tsx
@@ -2,7 +2,9 @@ import { TicketItem } from "@/features/ticket/components/TicketItem";
 import { getTicket } from "../queries/getTicket";
 import { NotFound } from "@/features/components/NotFound/NotFound";
 import { RedirectToast } from "@/components/redirect-toast";
-import { TicketProps } from "@/features/ticket/components/TicketItem/TicketItem";
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import { homePath } from "@/app/utils/paths";
+import { Separator } from "@/components/ui/separator";
 
 type TicketPageProps = {
   params: Promise<{
@@ -19,11 +21,18 @@ export default async function TicketPage({ params }: TicketPageProps) {
     return <NotFound />;
   }
   return (
-    <>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Breadcrumbs
+        breadcrumbs={[
+          { title: "Tickets", href: homePath },
+          { title: foundTicket.title },
+        ]}
+      />
+      <Separator />
       <div className="flex justify-center animate-fade-from-top">
         <TicketItem ticket={foundTicket} isDetail />
       </div>
       <RedirectToast />
-    </>
+    </div>
   );
 }
